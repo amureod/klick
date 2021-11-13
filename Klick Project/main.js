f2a523a5ea7ddefc9782cb5a0948db1ddbe2098e
amureod <94234563+amureod@users.noreply.github.com>
2021-11-13 18:31:32 +0100

    Add files via upload

---
+++ Klick Project/main.js
@@ -0,0 +1,44 @@
+
+document.getElementById("btn").innerHTML = "Start";
+
+function klik(){
+    document.getElementById("btn").removeEventListener('click', klik);
+    var time1 = setTimeout(timer1, 1000);
+    var time2 = setTimeout(timer2, 2000);
+    var time3 = setTimeout(timer3, 3000);
+    var time4 = setTimeout(timer4, 4000);
+    function timer1(){
+        document.getElementById("btn").innerHTML="3";
+    }
+    function timer2(){
+        document.getElementById("btn").innerHTML="2";
+    }
+    function timer3(){
+        document.getElementById("btn").innerHTML="1";
+    }
+    function timer4(){
+        document.getElementById("btn").innerHTML="Click!!!";
+        var y = 0;
+        document.getElementById("btn").onclick = function(){
+            y+=1;
+            document.getElementById("cps").innerHTML = ("Clicks : " + String(y));
+            var time7 = setTimeout(timer7, 10000);
+            function timer7(){
+                document.getElementById("cps").innerHTML = ("CPS : " + String(y/10));
+                document.getElementById("btn").onclick = function(){
+                    this.onclick = false;
+                }
+            }
+        }
+    }
+}
+
+document.getElementById("btn").addEventListener('click', klik);
+
+/* In real time CPS, doesnt work
+var time6 = setInterval(timer6, 1000);
+                function timer6(){
+                for (let x=1; x<10; x++){
+                    document.getElementById("cps").innerHTML = ("CPS : " + String(y/x))
+                }
+*/
